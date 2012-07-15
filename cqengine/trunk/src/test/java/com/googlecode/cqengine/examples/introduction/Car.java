d0a6cf7f6bf63c94c7e03dae44e5b6f339d7a02a
niall <niall@npgall.com>
2012-07-15 02:42:03 +0000

    Introductory example.

--- cqengine/trunk/src/test/java/com/googlecode/cqengine/examples/introduction/Car.java
+++ cqengine/trunk/src/test/java/com/googlecode/cqengine/examples/introduction/Car.java
@@ -1,39 +1,46 @@
 package com.googlecode.cqengine.examples.introduction;
 
 import com.googlecode.cqengine.attribute.Attribute;
+import com.googlecode.cqengine.attribute.MultiValueAttribute;
 import com.googlecode.cqengine.attribute.SimpleAttribute;
+
+import java.util.List;
 
 /**
  * @author Niall Gallagher
  */
 public class Car {
-
     public final int carId;
-    public final String manufacturer;
-    public final String model;
+    public final String name;
+    public final String description;
+    public final List<String> features;
 
-    public Car(int carId, String manufacturer, String model) {
+    public Car(int carId, String name, String description, List<String> features) {
         this.carId = carId;
-        this.manufacturer = manufacturer;
-        this.model = model;
+        this.name = name;
+        this.description = description;
+        this.features = features;
     }
 
     @Override
     public String toString() {
-        return "Car{carId=" + carId + ", manufacturer='" + manufacturer + "', model='" + model + "'}";
+        return "Car{carId=" + carId + ", name='" + name + "', description='" + description + "', features=" + features + "}";
     }
 
     // -------------------------- Attributes --------------------------
-
     public static final Attribute<Car, Integer> CAR_ID = new SimpleAttribute<Car, Integer>("carId") {
         public Integer getValue(Car car) { return car.carId; }
     };
 
-    public static final Attribute<Car, String> MANUFACTURER = new SimpleAttribute<Car, String>("manufacturer") {
-        public String getValue(Car car) { return car.manufacturer; }
+    public static final Attribute<Car, String> NAME = new SimpleAttribute<Car, String>("name") {
+        public String getValue(Car car) { return car.name; }
     };
 
-    public static final Attribute<Car, String> MODEL = new SimpleAttribute<Car, String>("model") {
-        public String getValue(Car car) { return car.model; }
+    public static final Attribute<Car, String> DESCRIPTION = new SimpleAttribute<Car, String>("description") {
+        public String getValue(Car car) { return car.description; }
+    };
+
+    public static final Attribute<Car, String> FEATURES = new MultiValueAttribute<Car, String>("features") {
+        public List<String> getValues(Car car) { return car.features; }
     };
 }
--- cqengine/trunk/src/test/java/com/googlecode/cqengine/examples/introduction/Introduction.java
+++ cqengine/trunk/src/test/java/com/googlecode/cqengine/examples/introduction/Introduction.java
@@ -1,11 +1,17 @@
 package com.googlecode.cqengine.examples.introduction;
 
+import com.googlecode.concurrenttrees.radixreversed.ReversedRadixTree;
 import com.googlecode.cqengine.CQEngine;
 import com.googlecode.cqengine.IndexedCollection;
 import com.googlecode.cqengine.index.hash.HashIndex;
 import com.googlecode.cqengine.index.navigable.NavigableIndex;
+import com.googlecode.cqengine.index.radix.RadixTreeIndex;
 import com.googlecode.cqengine.index.radixreversed.ReversedRadixTreeIndex;
+import com.googlecode.cqengine.index.suffix.SuffixTreeIndex;
 import com.googlecode.cqengine.query.Query;
+import com.googlecode.cqengine.resultset.ResultSet;
+
+import java.util.Arrays;
 
 import static com.googlecode.cqengine.query.QueryFactory.*;
 /**
@@ -16,35 +22,36 @@ import static com.googlecode.cqengine.query.QueryFactory.*;
 public class Introduction {
 
     public static void main(String[] args) {
-        // -------------------------- Set up the collection --------------------------
         // Create an indexed collection (note: could alternatively use CQEngine.copyFrom() existing collection)...
         IndexedCollection<Car> cars = CQEngine.newInstance();
 
         // Add some indexes...
         cars.addIndex(NavigableIndex.onAttribute(Car.CAR_ID));
-        cars.addIndex(HashIndex.onAttribute(Car.MANUFACTURER));
-        cars.addIndex(ReversedRadixTreeIndex.onAttribute(Car.MODEL));
+        cars.addIndex(ReversedRadixTreeIndex.onAttribute(Car.NAME));
+        cars.addIndex(SuffixTreeIndex.onAttribute(Car.DESCRIPTION));
+        cars.addIndex(HashIndex.onAttribute(Car.FEATURES));
 
         // Add some objects to the collection...
-        cars.add(new Car(1, "Ford", "Focus"));
-        cars.add(new Car(2, "Ford", "Taurus"));
-        cars.add(new Car(3, "Honda", "Civic"));
+        cars.add(new Car(1, "ford focus", "great condition, low mileage", Arrays.asList("spare tyre", "sunroof")));
+        cars.add(new Car(2, "ford taurus", "dirty and unreliable, flat tyre", Arrays.asList("spare tyre", "radio")));
+        cars.add(new Car(3, "honda civic", "has a flat tyre and high mileage", Arrays.asList("radio")));
 
         // -------------------------- Run some queries --------------------------
-        System.out.println("Cars manufactured by Ford:");
-        for (Car car : cars.retrieve(equal(Car.MANUFACTURER, "Ford"))) {
-            System.out.println(car);
-        }
-
-        System.out.println("\nCars whose model is Focus OR whose manufacturer is Honda:");
-        Query<Car> query1 = or(equal(Car.MODEL, "Focus"), equal(Car.MANUFACTURER, "Honda"));
+        System.out.println("Cars whose name ends with 'vic' or whose id is less than 2:");
+        Query<Car> query1 = or(endsWith(Car.NAME, "vic"), lessThan(Car.CAR_ID, 2));
         for (Car car : cars.retrieve(query1)) {
             System.out.println(car);
         }
 
-        System.out.println("\nCars whose model ends with 's' AND whose id is greater than 1:");
-        Query<Car> query2 = and(endsWith(Car.MODEL, "s"), greaterThan(Car.CAR_ID, 1));
+        System.out.println("\nCars whose flat tyre can be replaced:");
+        Query<Car> query2 = and(contains(Car.DESCRIPTION, "flat tyre"), equal(Car.FEATURES, "spare tyre"));
         for (Car car : cars.retrieve(query2)) {
+            System.out.println(car);
+        }
+
+        System.out.println("\nCars which have a sunroof or a radio but are not dirty:");
+        Query<Car> query3 = and(in(Car.FEATURES, "sunroof", "radio"), not(contains(Car.DESCRIPTION, "dirty")));
+        for (Car car : cars.retrieve(query3)) {
             System.out.println(car);
         }
     }
