9a09b21d5fefc2933a4201c9b3aa29cb9861c75f
niall <niall@npgall.com>
2012-07-18 22:00:49 +0000

    Added open source license header.

--- cqengine/trunk/src/test/java/com/googlecode/cqengine/examples/introduction/Introduction.java
+++ cqengine/trunk/src/test/java/com/googlecode/cqengine/examples/introduction/Introduction.java
@@ -1,15 +1,27 @@
+/**
+ * Copyright 2012 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.googlecode.cqengine.examples.introduction;
 
-import com.googlecode.concurrenttrees.radixreversed.ReversedRadixTree;
 import com.googlecode.cqengine.CQEngine;
 import com.googlecode.cqengine.IndexedCollection;
 import com.googlecode.cqengine.index.hash.HashIndex;
 import com.googlecode.cqengine.index.navigable.NavigableIndex;
-import com.googlecode.cqengine.index.radix.RadixTreeIndex;
 import com.googlecode.cqengine.index.radixreversed.ReversedRadixTreeIndex;
 import com.googlecode.cqengine.index.suffix.SuffixTreeIndex;
 import com.googlecode.cqengine.query.Query;
-import com.googlecode.cqengine.resultset.ResultSet;
 
 import java.util.Arrays;
 
