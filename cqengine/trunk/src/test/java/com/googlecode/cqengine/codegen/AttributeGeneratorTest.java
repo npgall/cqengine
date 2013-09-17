df048d5e5f6bdb80faf6e053300059f63360e7d7
niall@npgall.com <niall@npgall.com@0e031189-be9a-c2fb-fc0f-8e5d3f3506e4>
2013-09-17 23:05:04 +0000

    Additional work on AttributeGenerator, added support to generate MultiValueAttributes for fields which are Lists or arrays, also examine access modifiers, some unit tests (issue 22).

--- cqengine/trunk/src/test/java/com/googlecode/cqengine/codegen/AttributeGeneratorTest.java
+++ cqengine/trunk/src/test/java/com/googlecode/cqengine/codegen/AttributeGeneratorTest.java
@@ -1,15 +1,109 @@
 package com.googlecode.cqengine.codegen;
 
-import com.googlecode.cqengine.testutil.Car;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
 
 /**
  * @author Niall Gallagher
  */
 public class AttributeGeneratorTest {
 
-    public static void main(String[] args) {
-        System.out.println(AttributeGenerator.generateAttributesForClass(Car.class, false));
+    static class CarParent {
+        private int inaccessible;
+        protected int inheritedCarId;
+    }
+    static class Car extends CarParent {
+        String name;
+        private String description;
+        List<String> features;
+        double[] prices;
     }
 
+    @Test
+    public void testGenerateAttributesForPastingIntoTargetClass() {
+        String expected = "\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.name}.\n" +
+                "     */\n" +
+                "    public static final SimpleAttribute<Car, String> NAME = new SimpleAttribute<Car, String>(\"NAME\") {\n" +
+                "        public String getValue(Car car) { return car.name; }\n" +
+                "    };\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.description}.\n" +
+                "     */\n" +
+                "    public static final SimpleAttribute<Car, String> DESCRIPTION = new SimpleAttribute<Car, String>(\"DESCRIPTION\") {\n" +
+                "        public String getValue(Car car) { return car.description; }\n" +
+                "    };\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.features}.\n" +
+                "     */\n" +
+                "    public static final MultiValueAttribute<Car, String> FEATURES = new MultiValueAttribute<Car, String>(\"FEATURES\") {\n" +
+                "        public List<String> getValues(Car car) { return car.features; }\n" +
+                "    };\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.prices}.\n" +
+                "     */\n" +
+                "    public static final MultiValueAttribute<Car, Double> PRICES = new MultiValueAttribute<Car, Double>(\"PRICES\") {\n" +
+                "        public List<Double> getValues(final Car car) {\n" +
+                "            return new AbstractList<Double>() {\n" +
+                "                public Double get(int i) { return car.prices[i]; }\n" +
+                "                public int size() { return car.prices.length; }\n" +
+                "            };\n" +
+                "        }\n" +
+                "    };\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.inheritedCarId}.\n" +
+                "     */\n" +
+                "    public static final SimpleAttribute<Car, Integer> INHERITED_CAR_ID = new SimpleAttribute<Car, Integer>(\"INHERITED_CAR_ID\") {\n" +
+                "        public Integer getValue(Car car) { return car.inheritedCarId; }\n" +
+                "    };";
+        Assert.assertEquals(expected, AttributeGenerator.generateAttributesForPastingIntoTargetClass(Car.class));
+    }
 
+    @Test
+    public void testGenerateSeparateAttributesClass() {
+        String expected = "public class CarAttributes {\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.name}.\n" +
+                "     */\n" +
+                "    public static final SimpleAttribute<Car, String> NAME = new SimpleAttribute<Car, String>(\"NAME\") {\n" +
+                "        public String getValue(Car car) { return car.name; }\n" +
+                "    };\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.features}.\n" +
+                "     */\n" +
+                "    public static final MultiValueAttribute<Car, String> FEATURES = new MultiValueAttribute<Car, String>(\"FEATURES\") {\n" +
+                "        public List<String> getValues(Car car) { return car.features; }\n" +
+                "    };\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.prices}.\n" +
+                "     */\n" +
+                "    public static final MultiValueAttribute<Car, Double> PRICES = new MultiValueAttribute<Car, Double>(\"PRICES\") {\n" +
+                "        public List<Double> getValues(final Car car) {\n" +
+                "            return new AbstractList<Double>() {\n" +
+                "                public Double get(int i) { return car.prices[i]; }\n" +
+                "                public int size() { return car.prices.length; }\n" +
+                "            };\n" +
+                "        }\n" +
+                "    };\n" +
+                "\n" +
+                "    /**\n" +
+                "     * CQEngine attribute for field {@code Car.inheritedCarId}.\n" +
+                "     */\n" +
+                "    public static final SimpleAttribute<Car, Integer> INHERITED_CAR_ID = new SimpleAttribute<Car, Integer>(\"INHERITED_CAR_ID\") {\n" +
+                "        public Integer getValue(Car car) { return car.inheritedCarId; }\n" +
+                "    };\n" +
+                "}\n";
+        Assert.assertEquals(expected, AttributeGenerator.generateSeparateAttributesClass(Car.class, Car.class.getPackage().toString()));
+    }
 }
