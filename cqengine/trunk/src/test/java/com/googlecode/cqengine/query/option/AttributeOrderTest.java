41e2af88a2cc680328d39102c1e333710afaa16e
niall@npgall.com <niall@npgall.com@0e031189-be9a-c2fb-fc0f-8e5d3f3506e4>
2015-04-07 22:19:43 +0000

    Updated dates in license headers.

--- cqengine/trunk/src/test/java/com/googlecode/cqengine/query/option/AttributeOrderTest.java
+++ cqengine/trunk/src/test/java/com/googlecode/cqengine/query/option/AttributeOrderTest.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2012 Niall Gallagher
+ * Copyright 2012-2015 Niall Gallagher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
