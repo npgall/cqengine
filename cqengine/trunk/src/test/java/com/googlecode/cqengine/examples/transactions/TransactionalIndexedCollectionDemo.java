03bf1606d92598dfd8ea44a89407e383aab9074f
niall@npgall.com <niall@npgall.com@0e031189-be9a-c2fb-fc0f-8e5d3f3506e4>
2015-04-19 11:47:10 +0000

    Usage example for TransactionalIndexedCollection.

--- cqengine/trunk/src/test/java/com/googlecode/cqengine/examples/transactions/TransactionalIndexedCollectionDemo.java
+++ cqengine/trunk/src/test/java/com/googlecode/cqengine/examples/transactions/TransactionalIndexedCollectionDemo.java
@@ -2,11 +2,12 @@ package com.googlecode.cqengine.examples.transactions;
 
 import com.googlecode.cqengine.IndexedCollection;
 import com.googlecode.cqengine.TransactionalIndexedCollection;
+import com.googlecode.cqengine.query.QueryFactory;
 import com.googlecode.cqengine.resultset.ResultSet;
 import com.googlecode.cqengine.testutil.Car;
 import com.googlecode.cqengine.testutil.CarFactory;
 
-import static com.googlecode.cqengine.query.QueryFactory.all;
+import static com.googlecode.cqengine.query.QueryFactory.equal;
 import static java.util.Arrays.asList;
 
 /**
@@ -18,10 +19,10 @@ public class TransactionalIndexedCollectionDemo {
 
     public static void main(String[] args) {
         // Create example Car objects...
-        Car car1 = CarFactory.createCar(1);
-        Car car2 = CarFactory.createCar(2);
-        Car car3 = CarFactory.createCar(3);
-        Car car4 = CarFactory.createCar(4);
+        Car car1 = CarFactory.createCar(1); // "Ford Fusion"
+        Car car2 = CarFactory.createCar(2); // "Ford Taurus"
+        Car car3 = CarFactory.createCar(3); // "Honda Civic"
+        Car car4 = CarFactory.createCar(4); // "Honda Accord"
 
         // We will store the cars in TransactionalIndexedCollection, which provides MVCC support...
         IndexedCollection<Car> cars = new TransactionalIndexedCollection<Car>(Car.class);
@@ -39,15 +40,15 @@ public class TransactionalIndexedCollectionDemo {
 
         // ===== Examples of querying the collection using MVCC transactions... =====
 
-        // Read all cars with READ_COMMITTED transaction isolation...
-        ResultSet<Car> allCars = cars.retrieve(all(Car.class));
+        // Retrieve with READ_COMMITTED transaction isolation...
+        ResultSet<Car> resultSet = cars.retrieve(equal(Car.MANUFACTURER, "Ford"));
         try {
-            for (Car car : allCars) {
-                System.out.println(car); // prints cars 1, 3 & 4
+            for (Car car : resultSet) {
+                System.out.println(car); // prints car 1 ("Ford Fusion")
             }
         }
         finally {
-            allCars.close(); // ..close the ResultSet when finished reading!
+            resultSet.close(); // ..close the ResultSet when finished reading!
         }
     }
 }
