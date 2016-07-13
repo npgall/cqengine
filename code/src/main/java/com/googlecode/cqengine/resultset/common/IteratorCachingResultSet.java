97b5c9302555e6dcc38f97c15dedc9da322916a4
Niall Gallagher <niall@npgall.com>
2016-07-14 00:55:58 +0100

    Added IteratorCachingResultSet and unit tests.

---
+++ code/src/main/java/com/googlecode/cqengine/resultset/common/IteratorCachingResultSet.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright 2012-2015 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.cqengine.resultset.common;
+
+import com.googlecode.cqengine.query.Query;
+import com.googlecode.cqengine.query.option.QueryOptions;
+import com.googlecode.cqengine.resultset.ResultSet;
+
+import java.util.Iterator;
+
+/**
+ * A {@link ResultSet} which wraps a backing ResultSet, and which caches the iterator returned by
+ * the backing ResultSet, to enable repeated invocations on {@code IteratorCachingResultSet.iterator().hasNext()},
+ * to avoid each time requesting a new iterator from the backing ResultSet.
+ * <p/>
+ * An effect of this caching is that if the application calls {@link #isEmpty()} or {@link #isNotEmpty()} on
+ * this ResultSet before it begins iteration, only one iterator will actually be requested from the backing ResultSet
+ * because the cached iterator will be reused each time.
+ * <p/>
+ * On the other hand, whenever the application actually begins to iterate through results, this ResultSet will
+ * detect it, and if {@link #iterator()} is invoked again, it will avoid returning the same cached iterator
+ * and will obtain a new iterator instead.
+ *
+ * @author niall.gallagher
+ */
+public class IteratorCachingResultSet<O> extends ResultSet<O> {
+
+    final ResultSet<O> backingResultSet;
+
+    public IteratorCachingResultSet(ResultSet<O> backingResultSet) {
+        this.backingResultSet = backingResultSet;
+    }
+
+    Iterator<O> cachedIterator = null;
+
+
+    @Override
+    public Iterator<O> iterator() {
+        if (cachedIterator != null) {
+            return cachedIterator;
+        }
+        final Iterator<O> backingIterator = backingResultSet.iterator();
+        Iterator<O> wrappingIterator = new Iterator<O>() {
+
+            @Override
+            public boolean hasNext() {
+                return backingIterator.hasNext();
+            }
+
+            @Override
+            public O next() {
+                cachedIterator = null;
+                return backingIterator.next();
+            }
+
+            @Override
+            public void remove() {
+                cachedIterator = null;
+                backingIterator.remove();
+            }
+        };
+        this.cachedIterator = wrappingIterator;
+        return wrappingIterator;
+    }
+
+    @Override
+    public boolean contains(O object) {
+        return backingResultSet.contains(object);
+    }
+
+    @Override
+    public boolean matches(O object) {
+        return backingResultSet.matches(object);
+    }
+
+    @Override
+    public Query<O> getQuery() {
+        return backingResultSet.getQuery();
+    }
+
+    @Override
+    public QueryOptions getQueryOptions() {
+        return backingResultSet.getQueryOptions();
+    }
+
+    @Override
+    public int getRetrievalCost() {
+        return backingResultSet.getRetrievalCost();
+    }
+
+    @Override
+    public int getMergeCost() {
+        return backingResultSet.getMergeCost();
+    }
+
+    @Override
+    public int size() {
+        return backingResultSet.size();
+    }
+
+    @Override
+    public void close() {
+        backingResultSet.close();
+    }
+}
---
+++ code/src/test/java/com/googlecode/cqengine/resultset/common/IteratorCachingResultSetTest.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright 2012-2015 Niall Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.googlecode.cqengine.resultset.common;
+
+import com.googlecode.cqengine.resultset.ResultSet;
+import com.googlecode.cqengine.testutil.Car;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static com.googlecode.cqengine.testutil.CarFactory.createCar;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Mockito.*;
+
+/**
+ * Tests for {@link IteratorCachingResultSet}.
+ *
+ * @author niall.gallagher
+ */
+public class IteratorCachingResultSetTest {
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testIteratorCaching() {
+        ResultSet<Car> backingResultSet = mock(ResultSet.class);
+        when(backingResultSet.iterator()).thenReturn(mock(Iterator.class), mock(Iterator.class), mock(Iterator.class), mock(Iterator.class), mock(Iterator.class), mock(Iterator.class));
+        IteratorCachingResultSet<Car> iteratorCachingResultSet = new IteratorCachingResultSet<Car>(backingResultSet);
+
+        Iterator<Car> i1 = iteratorCachingResultSet.iterator();
+        Iterator<Car> i2 = iteratorCachingResultSet.iterator();
+        assertSame(i1, i2);
+
+        i2.hasNext();
+        i2.hasNext();
+        Iterator<Car> i3 = iteratorCachingResultSet.iterator();
+        assertSame(i1, i3);
+
+        i3.next();
+        Iterator<Car> i4 = iteratorCachingResultSet.iterator();
+        assertNotSame(i3, i4);
+
+        i4.remove();
+        Iterator<Car> i5 = iteratorCachingResultSet.iterator();
+        assertNotSame(i4, i5);
+
+        i5.hasNext();
+        i5.hasNext();
+        Iterator<Car> i6 = iteratorCachingResultSet.iterator();
+        assertSame(i5, i6);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testDelegatingMethods() {
+        ResultSet<Car> backingResultSet = mock(ResultSet.class);
+        IteratorCachingResultSet<Car> iteratorCachingResultSet = new IteratorCachingResultSet<Car>(backingResultSet);
+
+        iteratorCachingResultSet.contains(createCar(1));
+        verify(backingResultSet, times(1)).contains(createCar(1));
+
+        iteratorCachingResultSet.matches(createCar(2));
+        verify(backingResultSet, times(1)).matches(createCar(2));
+
+        iteratorCachingResultSet.getQuery();
+        verify(backingResultSet, times(1)).getQuery();
+
+        iteratorCachingResultSet.getQueryOptions();
+        verify(backingResultSet, times(1)).getQueryOptions();
+
+        iteratorCachingResultSet.getRetrievalCost();
+        verify(backingResultSet, times(1)).getRetrievalCost();
+
+        iteratorCachingResultSet.getMergeCost();
+        verify(backingResultSet, times(1)).getMergeCost();
+
+        iteratorCachingResultSet.size();
+        verify(backingResultSet, times(1)).size();
+
+        iteratorCachingResultSet.close();
+        verify(backingResultSet, times(1)).close();
+    }
+}
