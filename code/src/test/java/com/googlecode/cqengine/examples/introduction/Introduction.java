4e482af3ad1cda91ba1d6a98827cc444e5dbf719
npgall <niall@npgall.com>
2018-09-15 03:20:05 +0100

    Updated examples for Java 8+ usage

--- code/src/test/java/com/googlecode/cqengine/examples/introduction/Introduction.java
+++ code/src/test/java/com/googlecode/cqengine/examples/introduction/Introduction.java
@@ -51,20 +51,15 @@ public class Introduction {
         // -------------------------- Run some queries --------------------------
         System.out.println("Cars whose name ends with 'vic' or whose id is less than 2:");
         Query<Car> query1 = or(endsWith(Car.NAME, "vic"), lessThan(Car.CAR_ID, 2));
-        for (Car car : cars.retrieve(query1)) {
-            System.out.println(car);
-        }
+        cars.retrieve(query1).forEach(System.out::println);
 
         System.out.println("\nCars whose flat tyre can be replaced:");
         Query<Car> query2 = and(contains(Car.DESCRIPTION, "flat tyre"), equal(Car.FEATURES, "spare tyre"));
-        for (Car car : cars.retrieve(query2)) {
-            System.out.println(car);
-        }
+        cars.retrieve(query2).forEach(System.out::println);
+
 
         System.out.println("\nCars which have a sunroof or a radio but are not dirty:");
         Query<Car> query3 = and(in(Car.FEATURES, "sunroof", "radio"), not(contains(Car.DESCRIPTION, "dirty")));
-        for (Car car : cars.retrieve(query3)) {
-            System.out.println(car);
-        }
+        cars.retrieve(query3).forEach(System.out::println);
     }
 }
